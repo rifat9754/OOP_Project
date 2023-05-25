05ca93ceefe7706e75757abed639e54c53a1d4b5
rifat9754 <tahmid2007097@stud.kuet.ac.bd>
2023-05-25 23:42:57 +0600

    Initial Commit

---
+++ OOP_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP_Project
+//
+//  Created by Ahanaf Tahmid Rifat on 25/5/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
